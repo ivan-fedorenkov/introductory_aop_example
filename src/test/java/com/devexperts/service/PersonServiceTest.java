22ac1e15e2f10911b07bb252cb549080bcf6f9a1
Ivan Fedorenkov <ifedorenkov@devexperts.com>
2016-05-06 13:00:06 +0300

    draft

--- src/test/java/com/devexperts/service/PersonServiceTest.java
+++ src/test/java/com/devexperts/service/PersonServiceTest.java
@@ -6,6 +6,7 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import java.util.Objects;
@@ -17,34 +18,65 @@ import java.util.stream.StreamSupport;
  */
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringBootTest
+@DirtiesContext
 public class PersonServiceTest {
+
+    private static final String TEST_PERSON_NAME = "TEST_PERSON_NAME";
 
     @Autowired
     private PersonService personService;
 
     @Test
     public void testCreate() {
-        String personName = "TEST_PERSON_NAME";
 
         Iterable<Person> people = personService.findAll();
         Optional<Person> foundExisting = StreamSupport.stream(people.spliterator(), false)
-                .filter(p -> Objects.equals(personName, p.getName()))
+                .filter(p -> Objects.equals(TEST_PERSON_NAME, p.getName()))
                 .findFirst();
         Assert.assertFalse(foundExisting.isPresent());
 
 
-        Person person = new Person();
-        person.setName(personName);
+        Person person = createTestPerson(TEST_PERSON_NAME);
         Person created = personService.save(person);
 
         Assert.assertNotNull(created.getId());
-        Assert.assertEquals(personName, created.getName());
+        Assert.assertEquals(TEST_PERSON_NAME, created.getName());
 
         people = personService.findAll();
         Optional<Person> foundCreated = StreamSupport.stream(people.spliterator(), false)
-            .filter(p -> Objects.equals(personName, p.getName()))
+            .filter(p -> Objects.equals(TEST_PERSON_NAME, p.getName()))
             .findFirst();
         Assert.assertTrue(foundCreated.isPresent());
+    }
+
+    @Test
+    public void testUpdate() {
+        Person person = createTestPerson(TEST_PERSON_NAME);
+        person = personService.save(person);
+
+        String updatedName = "UPDATED_PERSON_NAME";
+        person.setName(updatedName);
+        personService.save(person);
+
+        Person updatedPerson = personService.findOne(person.getId()).orElseThrow(RuntimeException::new);
+        Assert.assertEquals(updatedName, updatedPerson.getName());
+    }
+
+    @Test
+    public void testRemove() {
+        Person person = createTestPerson(TEST_PERSON_NAME);
+        person = personService.save(person);
+
+        personService.remove(person);
+
+        Optional<Person> removed = personService.findOne(person.getId());
+        Assert.assertFalse(removed.isPresent());
+    }
+
+    private static Person createTestPerson(String name) {
+        Person p = new Person();
+        p.setName(name);
+        return p;
     }
 
 }
