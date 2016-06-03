71a42a5f30c544448d0668ee32fc7e49074234d9
Ivan Fedorenkov <ifedorenkov@devexperts.com>
2016-06-03 12:54:32 +0300

    performance test - aspectj (without load emulation)

--- src/main/java/com/devexperts/aop/AspectForBenchmark.java
+++ src/main/java/com/devexperts/aop/AspectForBenchmark.java
@@ -12,18 +12,7 @@ public class AspectForBenchmark {
 
     @Around(value = "within(com.devexperts.service..*) && execution(* *(..)) && @annotation(logged)")
     public Object doAround(ProceedingJoinPoint pjp, Logged logged) throws Throwable {
-        try {
-            Thread.sleep(1);
-        } catch (InterruptedException ie) {
-            throw new RuntimeException(ie);
-        }
-        Object result =  pjp.proceed();
-        try {
-            Thread.sleep(1);
-        } catch (InterruptedException ie) {
-            throw new RuntimeException(ie);
-        }
-        return result;
+        return pjp.proceed();
     }
 
 }
